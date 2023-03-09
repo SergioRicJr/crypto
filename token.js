aad78f15fdf770210ea93c840fd12d4a6757f458
Sergio <sergioricjr7@gmail.com>
2023-03-08 22:51:45 -0300

    tokens

---
+++ token.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+//chave secreta
+//pode ser usada com random bytes entre outras coisas
+const chaveSecreta = 'chaveSuperSecreta'
+
+
+//primeiro parametro são os dados e o segundo a chave
+const token = jwt.sign({
+    apelido: "jm",
+    curso: "segurança e node.js"
+}, chaveSecreta)
+
+console.log(token)
+
+const tokenDecodificado = jwt.verify(token, chaveSecreta)
+
+console.log(tokenDecodificado)
